7089f519d8ad12620db5679bf64f59ebbd1fef66
Austin Dworaczyk Wiltshire <561689+adworacz@users.noreply.github.com>
2024-07-16 19:35:54 -0700

    Start migrating to API v4 - starting with Super.
    I commented out all of the other functions just to ensure
    I could get everything compiling and working.
    
    So far it seems to be good.

--- src/EntryPoint.c
+++ src/EntryPoint.c
@@ -1,4 +1,5 @@
-#include <VapourSynth.h>
+#include <VapourSynth4.h>
+#include <stdlib.h>
 
 #include "CPU.h"
 
@@ -6,43 +7,47 @@
 // Extra indirection to keep the parameter lists with the respective filters.
 
 
-void mvsuperRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvanalyseRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvdegrainsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvcompensateRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvrecalculateRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvmaskRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvfinestRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvflowRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvflowblurRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvflowinterRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvflowfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvblockfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvscdetectionRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
-void mvdepanRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);
+void mvsuperRegister(VSPlugin *plugin, const VSPLUGINAPI *vspapi);
+/*void mvanalyseRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvdegrainsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvcompensateRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvrecalculateRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvmaskRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvfinestRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvflowRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvflowblurRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvflowinterRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvflowfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvblockfpsRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvscdetectionRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
+/*void mvdepanRegister(VSRegisterFunction registerFunc, VSPlugin *plugin);*/
 
 
 uint32_t g_cpuinfo = 0;
 
 
+//TODO: Update this.
 VS_EXTERNAL_API(void)
-VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
-    configFunc("com.nodame.mvtools", "mv", "MVTools v" PACKAGE_VERSION, VAPOURSYNTH_API_VERSION, 1, plugin);
+/*VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {*/
+VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
+    const int packageVersion = atoi(PACKAGE_VERSION);
 
-    mvsuperRegister(registerFunc, plugin);
-    mvanalyseRegister(registerFunc, plugin);
-    mvdegrainsRegister(registerFunc, plugin);
-    mvcompensateRegister(registerFunc, plugin);
-    mvrecalculateRegister(registerFunc, plugin);
-    mvmaskRegister(registerFunc, plugin);
-    mvfinestRegister(registerFunc, plugin);
-    mvflowRegister(registerFunc, plugin);
-    mvflowblurRegister(registerFunc, plugin);
-    mvflowinterRegister(registerFunc, plugin);
-    mvflowfpsRegister(registerFunc, plugin);
-    mvblockfpsRegister(registerFunc, plugin);
-    mvscdetectionRegister(registerFunc, plugin);
-    mvdepanRegister(registerFunc, plugin);
+    vspapi->configPlugin("com.nodame.mvtools", "mv", "MVTools v" PACKAGE_VERSION, VS_MAKE_VERSION(packageVersion, 0), VS_MAKE_VERSION(VAPOURSYNTH_API_MAJOR, VAPOURSYNTH_API_MINOR), 0, plugin);
+
+    mvsuperRegister(plugin, vspapi);
+    /*mvanalyseRegister(registerFunc, plugin);*/
+    /*mvdegrainsRegister(registerFunc, plugin);*/
+    /*mvcompensateRegister(registerFunc, plugin);*/
+    /*mvrecalculateRegister(registerFunc, plugin);*/
+    /*mvmaskRegister(registerFunc, plugin);*/
+    /*mvfinestRegister(registerFunc, plugin);*/
+    /*mvflowRegister(registerFunc, plugin);*/
+    /*mvflowblurRegister(registerFunc, plugin);*/
+    /*mvflowinterRegister(registerFunc, plugin);*/
+    /*mvflowfpsRegister(registerFunc, plugin);*/
+    /*mvblockfpsRegister(registerFunc, plugin);*/
+    /*mvscdetectionRegister(registerFunc, plugin);*/
+    /*mvdepanRegister(registerFunc, plugin);*/
 
     g_cpuinfo = cpu_detect();
 }
